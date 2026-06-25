01def9e2229a31ed87f6f01b5b376cab00c4f314
kidrauhl123 <755439274@qq.com>
2026-06-25 21:28:38 +0800

    fix(win): align desktop chrome surfaces

--- src/main/windows-title-bar.js
+++ src/main/windows-title-bar.js
@@ -1,6 +1,7 @@
 const WINDOWS_TITLE_BAR_HEIGHT = 32;
-const WINDOWS_LIGHT_TITLE_BAR_COLOR = "#e9edf2";
-const WINDOWS_DARK_TITLE_BAR_COLOR = "#20242b";
+const WINDOWS_TITLE_BAR_OVERLAY_HEIGHT = WINDOWS_TITLE_BAR_HEIGHT - 1;
+const WINDOWS_LIGHT_TITLE_BAR_COLOR = "#f2f4f7";
+const WINDOWS_DARK_TITLE_BAR_COLOR = "#20232a";
 const WINDOWS_LIGHT_SYMBOL_COLOR = "#24262d";
 const WINDOWS_DARK_SYMBOL_COLOR = "#f2f4f8";
 
@@ -9,7 +10,7 @@ function windowsTitleBarOverlayForAppearance(appearance = {}) {
   return {
     color: theme === "dark" ? WINDOWS_DARK_TITLE_BAR_COLOR : WINDOWS_LIGHT_TITLE_BAR_COLOR,
     symbolColor: theme === "dark" ? WINDOWS_DARK_SYMBOL_COLOR : WINDOWS_LIGHT_SYMBOL_COLOR,
-    height: WINDOWS_TITLE_BAR_HEIGHT
+    height: WINDOWS_TITLE_BAR_OVERLAY_HEIGHT
   };
 }
 
@@ -26,6 +27,7 @@ function applyWindowsTitleBarOverlay(win, appearance = {}) {
 
 module.exports = {
   WINDOWS_TITLE_BAR_HEIGHT,
+  WINDOWS_TITLE_BAR_OVERLAY_HEIGHT,
   WINDOWS_LIGHT_TITLE_BAR_COLOR,
   WINDOWS_DARK_TITLE_BAR_COLOR,
   windowsTitleBarOverlayForAppearance,
